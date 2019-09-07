338b36c903adfb1d9184272b91fd34b2a7298260
VelociraptorCZE <raichls44@gmail.com>
2019-09-07 16:34:16 +0200

    indentation decreased to two spaces

--- minilazyload.js
+++ minilazyload.js
@@ -7,85 +7,84 @@
 export default function MiniLazyload (options = {}, selector, override) {
   const isIntersectionObserverNotSupported = () => !window.IntersectionObserver;
 
-	this.update = () => {
-		if (this.enabled) {
-			this.loadImages(() => {}, false);
-		}
-	};
+  this.update = () => {
+    if (this.enabled) {
+      this.loadImages(() => {}, false);
+    }
+  };
 
-	this.allElements = () => [].slice.call(document.querySelectorAll(this.selector));
+  this.allElements = () => [].slice.call(document.querySelectorAll(this.selector));
 
-	this.loadImages = (callback = () => {}, loadImmediately = true) => {
-		this.allElements().forEach(element => {
-			onEvents(element);
-			callback(element);
+  this.loadImages = (callback = () => {}, loadImmediately = true) => {
+    this.allElements().forEach(element => {
+      onEvents(element);
+      callback(element);
 
-			if (isIntersectionObserverNotSupported() || loadImmediately) {
-				loadImage(element);
-			}
-			else {
-				this.newObserver().observe(element);
-			}
-		});
-	};
+      if (isIntersectionObserverNotSupported() || loadImmediately) {
+        loadImage(element);
+      } else {
+        this.newObserver().observe(element);
+      }
+    });
+  };
 
-	this.newObserver = () => (
-		new IntersectionObserver((entries, observer) => {
-			entries.forEach(({ intersectionRatio, target }) => {
-				if (intersectionRatio > 0) {
-					observer.unobserve(target);
-					loadImage(target, true);
-				}
-			});
-		}, this.options)
-	);
+  this.newObserver = () => (
+    new IntersectionObserver((entries, observer) => {
+      entries.forEach(({ intersectionRatio, target }) => {
+        if (intersectionRatio > 0) {
+          observer.unobserve(target);
+          loadImage(target, true);
+        }
+      });
+    }, this.options)
+  );
 
-	const loadImage = (target, loadBg) => {
-		const { src, srcset, bg } = target.dataset;
+  const loadImage = (target, loadBg) => {
+    const { src, srcset, bg } = target.dataset;
 
-		if (src) {
-			target.src = src;
-		}
+    if (src) {
+      target.src = src;
+    }
 
-		if (srcset) {
-			target.srcset = srcset;
-		}
+    if (srcset) {
+      target.srcset = srcset;
+    }
 
-		if ((loadBg || isIntersectionObserverNotSupported()) && bg) {
-			target.style.backgroundImage = `url(${bg})`;
-		}
+    if ((loadBg || isIntersectionObserverNotSupported()) && bg) {
+      target.style.backgroundImage = `url(${bg})`;
+    }
 
-		translateSrcset(target.parentElement);
-	};
+    translateSrcset(target.parentElement);
+  };
 
-	const translateSrcset = element => {
-		if (window.HTMLPictureElement && element instanceof HTMLPictureElement) {
-			[].slice.call(element.querySelectorAll("[data-srcset]")).forEach(source => {
-				source.srcset = source.dataset.srcset;
-			});
-		}
-	};
+  const translateSrcset = element => {
+    if (window.HTMLPictureElement && element instanceof HTMLPictureElement) {
+      [].slice.call(element.querySelectorAll("[data-srcset]")).forEach(source => {
+        source.srcset = source.dataset.srcset;
+      });
+    }
+  };
 
-	const onEvents = element => {
-		const { placeholder } = this.options;
-		const loaded = () => element.classList.add("loaded");
+  const onEvents = element => {
+    const { placeholder } = this.options;
+    const loaded = () => element.classList.add("loaded");
 
-		element.addEventListener("error", () => {
-			if (placeholder && element.className.indexOf("error") === -1) {
-				element.src = placeholder;
-			}
-			element.classList.add("error");
-			element.removeEventListener("load", loaded);
-		});
+    element.addEventListener("error", () => {
+      if (placeholder && element.className.indexOf("error") === -1) {
+        element.src = placeholder;
+      }
+      element.classList.add("error");
+      element.removeEventListener("load", loaded);
+    });
 
-		element.addEventListener("load", loaded);
-	};
+    element.addEventListener("load", loaded);
+  };
 
-	this.selector = selector || "[loading=lazy]";
-	this.options = options;
-	this.enabled = !HTMLImageElement.prototype.hasOwnProperty("loading")
-		|| override === MiniLazyload.IGNORE_NATIVE_LAZYLOAD;
-	this.update();
+  this.selector = selector || "[loading=lazy]";
+  this.options = options;
+  this.enabled = !HTMLImageElement.prototype.hasOwnProperty("loading")
+    || override === MiniLazyload.IGNORE_NATIVE_LAZYLOAD;
+  this.update();
 }
 
 MiniLazyload.IGNORE_NATIVE_LAZYLOAD = true;
